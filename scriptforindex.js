74d6603fb28e6a0004d44dfe56ae6257e07b94cb
Haoyi Lin <1625397867@qq.com>
2024-10-05 21:10:38 -0500

    add index

--- scriptforindex.js
+++ scriptforindex.js
@@ -5,11 +5,17 @@ window.addEventListener('scroll', function() {
     const target3 = document.getElementById('target3').getBoundingClientRect().top;
     const target4 = document.getElementById('target4').getBoundingClientRect().top;
     const target5 = document.getElementById('target5').getBoundingClientRect().top;
+    const target6 = document.getElementById('target6').getBoundingClientRect().top;
+    const target7 = document.getElementById('target7').getBoundingClientRect().top;
+
+
+
     const bottom = document.getElementById('bottom').getBoundingClientRect().top;
 
     if ((target1 <= 40 && target2 >= 40) ||
         (target3 <= 40 && target4 >= 40) ||
-        (target5 <= 40 && bottom >= 40 )
+        (target5 <= 40 && target6 >= 40) ||
+        (target7 <= 40 && bottom >= 40 )
     ) {
         header.style.color = 'white'
     } else{
