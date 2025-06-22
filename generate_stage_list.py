9eb838ee791a42474e3cf4c8c6bc8a7ab59b2506
SableFiSh <1625397867@qq.com>
2025-06-22 00:15:38 -0400

    Add stage list and stage page generation functions

---
+++ generate_stage_list.py
@@ -0,0 +1,70 @@
+from generator_help_functions import *
+
+
+def get_stage_list_path():
+    """
+    Get the stage list path.
+    
+    :return: a list of stage list path
+    """
+
+
+def write_stage_list_head():
+    """
+    Write stage list head.
+    
+    :return: None
+    """
+
+
+def get_body_part_of_stage_list(stage_folder: Path, is_first: bool = False):
+    """
+    Get the body part of the stage list.
+    
+    :return: a string representing the body part of the stage list
+    """
+
+
+def get_all_stage_folders():
+    """
+    Get all stage folders.
+    
+    :return: a list of stage folder paths in date order
+    """
+
+
+def write_stage_list_body():
+    """
+    Write the body of the stage list.
+    
+    :return: None
+    """
+
+
+def write_stage_list_tail():
+    """
+    Write the tail of the stage list.
+    
+    :return: None
+    """
+
+
+def stage_list_part_return(part: int):
+    """
+    Return the head or tail of the stage list HTML.
+
+    :param part: 0 for head, 1 for head-zh, 2 for tail
+    :return: str of head or tail
+    """
+
+
+def generate_stage_list():
+    """
+    Generate the stage list HTML files.
+    
+    :return: None
+    """
+
+
+if __name__ == "__main__":
+    generate_stage_list()
---
+++ generate_stage_page.py
@@ -0,0 +1,67 @@
+from generator_help_functions import *
+
+
+def choose_stage_folder():
+    """
+    Get the folder path to translate.
+    
+    :return: the path of the folder.
+    """
+
+
+def write_stage_page_head(folder_path: Path):
+    """
+    Write both head of the stage page HTML file.
+
+    :param folder_path: the folder path
+    :return: None
+    """
+
+
+def txt_to_stage_body_translate(folder_path: Path, file_path: Path):
+    """
+    Translate the chosen file to HTML body.
+
+    :param folder_path: the folder path
+    :param file_path: the file path
+    :return: None
+    """
+
+
+def write_stage_page_body(folder_path: Path):
+    """
+    Write the body of the stage page HTML file.
+
+    :param folder_path: the folder path
+    :return: None
+    """
+
+
+def write_stage_page_tail(folder_path: Path):
+    """
+    Write the tail of the stage page HTML file.
+
+    :param folder_path: the folder path
+    :return: None
+    """
+
+    
+def stage_part_return(part: int):
+    """
+    Return the head or tail of the stage page HTML.
+
+    :param part: 0 for head, 1 for head-zh, 2 for tail
+    :return: str of head or tail
+    """
+
+
+def generate_stage_page():
+    """
+    Generate the stage page.
+    
+    :return: None
+    """
+    
+
+if __name__ == "__main__":
+    generate_stage_page()
