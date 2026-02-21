818d4d75888adb148990884a731a3b1e4f4f9252
SableFiSh <1625397867@qq.com>
2026-02-21 14:23:36 -0600

    change init

--- generator/__init__.py
+++ generator/__init__.py
@@ -4,8 +4,11 @@ import logging
 sys.stdout.reconfigure(encoding='utf-8') # type: ignore
 
 logging.basicConfig(
-    filename= "./generator/generator.log",
-    filemode= "w",
     level= logging.INFO,
-    format= "%(asctime)s - %(levelname)s - %(module)s.%(funcName)s - %(message)s"
+    format= "%(asctime)s - %(levelname)s - %(module)s.%(funcName)s - %(message)s",
+
+    handlers=[
+        logging.FileHandler("./generator/generator.log", mode='w', encoding='utf-8'),
+        logging.StreamHandler(sys.stdout)
+    ]
 )
